bada02d7f64876da00a52ffbb56a34084a00a859
Eduardo <eduardo.garcia@magmalabs.io>
2019-03-28 14:25:16 -0600

    Role in User model
    We want to have a readable way to manage the user roles. And We can get
    this using enums to represent integers as defined roles.
    
    This adds enum role to manage the User roles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
   has_many :posts
+
+  enum role: %w[writer moderator]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
